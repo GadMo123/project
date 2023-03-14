ee735d3f481dad46928ae47afdedb2647b888409
gad.morgenstern@gmail.com <gad.morgenstern@gmail.com>
2023-03-13 21:58:42 -0600

    change and commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int a = 0;
-        System.out.println(a);
+
+        try {
+            int port = 9000;
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("server started at " + port);
+            server.createContext("/boot-bootcamp", new GetHandler());
+            server.setExecutor(null);
+            server.start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
